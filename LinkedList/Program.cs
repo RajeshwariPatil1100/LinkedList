8da6c9963bf9ffe50b8a9d240d3f3044831eac60
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2022-12-22 18:32:41 +0530

    [Rajeshwari] Add Simple Linked List

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleLinkedList list = new SimpleLinkedList();
+            list.Addnote(56);
+            list.Addnote(30);
+            list.Addnote(70);
+            list.Display();
+
+        }
+    }
+}
---
+++ LinkedList/SimpleLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedList
+{
+    internal class SimpleLinkedList
+    {
+        Node head;//ref variable//head points to the 1st element of the linkedlist
+        public void Addnote(int data)
+        {
+            Node node = new Node(data);//every time create new object //Node-Contains key and pointer to its next Node
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} intrested into LInked List", node.data);
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("List contains");
+                Console.ResetColor();
+                Node tempnode = head;
+                while (tempnode != null)
+                {
+                    Console.WriteLine("data - " + tempnode.data + " ");
+                    tempnode = tempnode.Next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
